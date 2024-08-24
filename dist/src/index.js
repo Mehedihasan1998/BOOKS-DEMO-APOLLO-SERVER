9a000bae58e631eb1760bfc2ab2e0c497c74b3d5
Md. Mehedi Hasan <mehedi5477@gmail.com>
2024-08-24 15:30:18 +0600

    Initial commit

---
+++ dist/src/index.js
@@ -0,0 +1,65 @@
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone";
+import { connect } from 'mongoose';
+import Book from "../models/book.js";
+const MONGODB = 'mongodb+srv://root:root@cluster0.rrfl5.mongodb.net/Book?retryWrites=true&w=majority&appName=Cluster0';
+const typeDefs = `
+    type Book {
+        _id : String
+        author : String
+        title : String
+        year : Int
+        price : Int
+    }
+
+    input BookInput {
+        author : String
+        title : String
+        year : Int
+        price : Int
+    }
+    
+    type Query {
+        getBook(ID : ID!) : Book!
+        getBooks(limit : Int) : [Book]
+    }
+
+    type Mutation {
+        createBook(bookInput : BookInput) : String!
+        updateBook(ID : ID!, bookInput : BookInput) : String!
+        deleteBook(ID : ID!) : String!
+    }
+`;
+const resolvers = {
+    Query: {
+        async getBook(_, { ID }) {
+            return await Book.findById(ID);
+        },
+        async getBooks(_, { limit }) {
+            return await Book.find().limit(limit);
+        },
+    },
+    Mutation: {
+        async createBook(_, { bookInput: { author, title, year, price } }) {
+            const res = await new Book({ author, title, year, price }).save();
+            return res._id;
+        },
+        async updateBook(_, { ID, bookInput: { author, title, year, price } }) {
+            await Book.updateOne({ _id: ID }, { $set: { author, title, year, price } });
+            return ID;
+        },
+        async deleteBook(_, { ID }) {
+            await Book.deleteOne({ _id: ID });
+            return ID;
+        }
+    }
+};
+await connect(MONGODB);
+const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+});
+const { url } = await startStandaloneServer(server, {
+    listen: { port: 4000 }
+});
+console.log(`Server is ready at ${url}`);
